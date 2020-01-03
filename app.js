f9ab8035f4c1a46e180afc5393364ae2027b0328
mimiko 117 <scm.mimiko@gmail.com>
2020-01-03 09:23:00 +0630

    remove config

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 app.use('/user', Router.UserRoute);
 app.use('/post', Router.PostRoute);
 app.use('/excel', Router.ExcelRoute);
 app.use('/comment', Router.CommentRoute);
 
 app.listen(3000, () => {
-  console.log('Go to http://localhost:3000 to run queries!');
+  console.log('Server is up on http://localhost:3000 !');
 });
 
 module.exports = app;
