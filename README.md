564091e6f966f9f11d021491b12afb15c9fa7d03
mimiko 117 <scm.mimiko@gmail.com>
2020-01-07 10:07:32 +0630

    fix review points

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Bulletinboard-API-Express
+
+#Project Setup
+
+###How to start local server
+First time only, please run following commands.
+npm install
+Then,you can run the project with following command.
+npm run dev
+
+###Import Data to MySql Database
+You can use following file to import database.
+ importData.sql
+
+###DefaultAdminUser 
+You can Login with the following user after imported sql file.
+email - scm.mimiko@gmail.com
+password - 105243mc
+
+###Local Endpoint
+http://localhost:3000
--- app.js
+++ app.js
@@ -2,17 +2,20 @@ const express = require('express');
 const Router = require('./routes/index');
 const cors = require('cors');
 const app = express();
-
+const morgan = require('morgan');
+const http = require('http');
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
+app.use(morgan('dev'));
 app.use('/user', Router.UserRoute);
 app.use('/post', Router.PostRoute);
 app.use('/excel', Router.ExcelRoute);
 app.use('/comment', Router.CommentRoute);
-
-app.listen(3000, () => {
-  console.log('Server is up on http://localhost:3000 !');
+const server = http.createServer(app);
+server.listen(3000, () => {
+  console.log('Go to http://localhost:3000 to run queries!');
 });
+
 
 module.exports = app;
